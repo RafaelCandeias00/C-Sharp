c4978bfa5ef61640426cf1bf01e82d91bc3a5f4a
Rafael Candeias <rafaelcandeias39@gmail.com>
2021-01-11 19:51:02 -0300

    C#

---
+++ Medidas do triangulo/Seção 4 - Aula 45 - Exercício 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Seção_4___Aula_45___Exercício_1 {
+    class Program {
+        static void Main(string[] args) {
+            Retangulo medida;
+            medida = new Retangulo();
+
+            Console.Write("Entre com a largura: ");
+            medida.medidaLargura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Entre com a altura: ");
+            medida.medidaAltura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            medida.ToString();
+            Console.WriteLine($"{medida}");
+        }
+    }
+}
---
+++ Reajuste de salário/Seção 4 - Aula 45 - Exercício 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Seção_4___Aula_45___Exercício_2 {
+    class Program {
+        static void Main(string[] args) {
+            Funcionario func;
+            func = new Funcionario();
+
+            double acressentarImposto;
+
+            Console.Write("Nome: ");
+            func.nomeFuncionario = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            func.salarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("");
+            Console.WriteLine($"Funcionário: {func.nomeFuncionario}, R${func.SalarioLiquido()}");
+
+            Console.WriteLine("");
+            Console.Write($"Digite a porcentagem para aumentar o salário: ");
+            acressentarImposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(acressentarImposto);
+
+            Console.WriteLine($"{func}");
+        }
+    }
+}
