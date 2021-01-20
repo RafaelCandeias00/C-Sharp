b123f23779a9866a01fed4655970f1c6eacdc576
Rafael Candeias <rafaelcandeias39@gmail.com>
2021-01-20 11:49:14 -0300

    C#

---
+++ Pesquisa pelo ID/Seção 6 - Aula 78 - Exercício de fixação/Empregado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Seção_6___Aula_78___Exercício_de_fixação {
+    class Empregado {
+        public int IdEmpregado { get; set; }
+        public string NomeEmpregado { get; set; }
+        public double SalarioEmpregado { get; set; }
+
+        public Empregado(int id, string nome, double salario) {
+            IdEmpregado = id;
+            NomeEmpregado = nome;
+            SalarioEmpregado = salario;
+        }
+
+        public void acressimoSalario(double salario) {
+            SalarioEmpregado += (salario / 100 * SalarioEmpregado);
+        }
+
+        public override string ToString() {
+            return $"{IdEmpregado}, {NomeEmpregado}, R${SalarioEmpregado.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Pesquisa pelo ID/Seção 6 - Aula 78 - Exercício de fixação/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Seção_6___Aula_78___Exercício_de_fixação {
+    class Program {
+        static void Main(string[] args) {
+            List<Empregado> list = new List<Empregado>();
+
+            Console.Write("Quantos funcionários serão registrados? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine($"Empregado #{i}");
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salário: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Empregado(id, nome, salario));
+                Console.WriteLine();
+            }
+
+            Console.Write("Coloque o ID do empregado que terá aumento de salário: ");
+            int idPesquisa = int.Parse(Console.ReadLine());
+
+            Empregado emp = list.Find(x => x.IdEmpregado == idPesquisa);
+            if (emp != null) {
+                Console.Write("Entre com a porcentagem: ");
+                double acressimo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.acressimoSalario(acressimo);
+            } else {
+                Console.WriteLine("ID incorreto");
+            }
+
+
+            Console.WriteLine("Dados atualizados: ");
+            foreach (Empregado obj in list) {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
