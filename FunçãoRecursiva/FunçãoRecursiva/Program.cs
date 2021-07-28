b8c636819a4e4d8f48e65c9c9cdb09b7388e0d97
Rafael Candeias <rafaelcandeias39@gmail.com>
2021-07-28 18:32:43 -0300

    C#

---
+++ FunçãoRecursiva/FunçãoRecursiva/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FunçãoRecursiva
+{
+    class Program
+    {
+        private static void LerArquivo(int numeroArquivo)
+        {
+            string arquivoComCaminho = @"C:\arquivos\arq"+ numeroArquivo +".txt";
+            if (File.Exists(arquivoComCaminho))
+            {
+                using (StreamReader arquivo = File.OpenText(arquivoComCaminho))
+                {
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+                    }
+                }
+            }
+            string arquivoComCaminho2 = @"C:\arquivos\arq" + (numeroArquivo + 1) + ".txt";
+            if (File.Exists(arquivoComCaminho2))
+            {
+                LerArquivo(numeroArquivo + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            LerArquivo(1);
+        }
+    }
+}
